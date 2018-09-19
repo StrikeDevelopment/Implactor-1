c5929321d35d67f88112cccc0e841736336b6427
Zadey <41181506+Zadezter@users.noreply.github.com>
2018-09-19 22:34:15 +0800

    Update EntityManager.php

--- src/Implactor/EntityManager.php
+++ src/Implactor/EntityManager.php
@@ -25,8 +25,9 @@ declare(strict_types=1);
 namespace Implactor;
 
 use pocketmine\Player;
-use pocketmine\entity\Entity;
+use pocketmine\entity\{Entity, Creature, Human};
 use pocketmine\nbt\NBT;
+use pocketmine\network\mcpe\protocol\AddEntityPacket;
 use pocketmine\nbt\tag\{CompoundTag, ListTag, DoubleTag, FloatTag, NamedTag};
 use pocketmine\math\Vector3;
 
@@ -37,6 +38,7 @@ use Implactor\listeners\EventListener;
 
 class EntityManager {
 
+  public $exemptedEntities = [];
   private static $customEntities;
   
   public function __construct() {
@@ -93,5 +95,66 @@ class EntityManager {
       $death->spawnToAll();
       self::getCustom()->getScheduler()->scheduleDelayedTask(new DeathHumanDespawnTask(Implade::getInstance(), $death, $player), 1000);
     }
+  }
+  
+  public function isSummonLightning(Player $player, $strike): void {
+    if ($strike === true) {
+      $level = $player->getLevel();
+          
+      $thunder = new AddEntityPacket();
+      $thunder->type = 93;
+      $thunder->entityRuntimeId = Entity::$entityCount++;
+      $thunder->position = $player->asVector3()->add(0, 0);
+      $thunder->yaw = $player->getYaw();
+      $thunder->pitch = $player->getPitch();
+      $player->getServer()->broadcastPacket($player->getServer()->getOnlinePlayers(), $thunder);
+    }
+  }
+  
+  public function clearCorpses(): int {
+    $corpses = 0;
+    foreach (Implade::getInstance()->getServer()->getLevels() as $level) {
+      foreach ($level->getEntities() as $entity) {
+        if ($entity instanceof DeathHuman) {
+          $entity->close();
+          $corpses++;
+        }
+      }
+    }
+    return $corpses;
+  }
+  
+  public function clearItems(): int {
+    $item = 0;
+    foreach (Implade::getInstance()->getServer()->getLevels() as $level) {
+      foreach ($level->getEntities() as $entity) {
+        if (!self::getCustom()->isEntityExempted($entity) && !($entity instanceof Creature)) {
+          $entity->close();
+          $item++;
+        }
+      }
+    }
+    return $item;
+  }
+
+  public function clearMobs(): int {
+    $mobs = 0;
+    foreach (Implade::getInstance()->getServer()->getLevels() as $level) {
+      foreach ($level->getEntities() as $entity) {
+        if (!self::getCustom()->isEntityExempted($entity) && $entity instanceof Creature && !($entity instanceof Human)) {
+          $entity->close();
+          $mobs++;
+        }
+      }
+    }
+    return $mobs;
+  }
+
+  public function exemptEntity(Entity $entity): void {
+    self::getCustom()->exemptedEntities[$entity->getID()] = $entity;
+  }
+
+  public function isEntityExempted(Entity $entity): bool {
+    return isset(self;:getCustom()->exemptedEntities[$entity->getID()]);
   }
 }
